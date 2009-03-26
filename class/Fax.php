cf861d62c87dad567e56f3a61fec05952e96dfd0
jbooker <devnull@localhost>
2009-03-26 15:44:37 +0000

    Added code for counting the number of pages in a fax.

--- class/Fax.php
+++ class/Fax.php
@@ -21,7 +21,9 @@ class Fax {
 
 
     /**
-     * Constructor for loading an existing fax
+     * Constructor
+     * If passed an ID, it will attempt to load the existing fax which has the given ID. Otherwise,
+     * the additional parameters are required in order to create a new Fax object.
      */
     public function __construct($id = 0, $senderPhone = NULL, $fileName = NULL, $dateReceived = NULL)
     {
@@ -283,6 +285,54 @@ class Fax {
         }
 
         return $result;
+    }
+
+    /**
+     * Returns the number of pages in the given fax object
+     */
+    public static function countPages($fax)
+    {
+        if(!file_exists($fax->getFileName())){
+            return null;
+        }
+
+        //open the file for reading
+        $handle = @fopen($fax->getFileName(), "rb");
+
+        if(!$handle){
+            return null;
+        }
+
+        $count = 0;
+        $i=0;
+
+        while (!feof($handle)) {
+            if($i > 0) {
+                $contents .= fread($handle,8152);
+            }else{
+                $contents = fread($handle, 1000);
+                //In some pdf files, there is an N tag containing the number of
+                //of pages. This doesn't seem to be a result of the PDF version.
+                //Saves reading the whole file.
+                if(preg_match("/\/N\s+([0-9]+)/", $contents, $found)) {
+                    return $found[1];
+                }
+            }
+            $i++;
+        }
+
+        fclose($handle);
+
+        //get all the trees with 'pages' and 'count'. the biggest number
+        //is the total number of pages, if we couldn't find the /N switch above.                
+        if(preg_match_all("/\/Type\s*\/Pages\s*.*\s*\/Count\s+([0-9]+)/", $contents, $capture, PREG_SET_ORDER)) {
+            foreach($capture as $c) {
+                if($c[1] > $count)
+                $count = $c[1];
+            }
+            return $count;            
+        }
+        return 0; 
     }
 }
 
